5c299a12863e679646589c0f6f1bb2d642d7c43e
jannand09 <josephrannand@gmail.com>
2025-01-29 17:00:29 -0500

    Update Lab 3 Progress
    Created SQL DDL for Exercise 6.8

---
+++ Week3/Labs/exercise_68.sql
@@ -0,0 +1,70 @@
+DROP TABLE IF EXISTS lib.book;
+DROP TABLE IF EXISTS lib.book_authors;
+DROP TABLE IF EXISTS lib.publisher;
+DROP TABLE IF EXISTS lib.book_copies;
+DROP TABLE IF EXISTS lib.book_loans;
+DROP TABLE IF EXISTS lib.library_branch;
+DROP TABLE IF EXISTS lib.borrower;
+DROP SCHEMA IF EXISTS lib;
+
+CREATE SCHEMA IF NOT EXISTS lib;
+
+CREATE TABLE IF NOT EXISTS lib.publisher (
+	Publisher_name VARCHAR NOT NULL,
+	Address VARCHAR,
+	Phone VARCHAR,
+	PRIMARY KEY (Publisher_name)
+);
+
+CREATE TABLE IF NOT EXISTS lib.borrower (
+	Card_no INT NOT NULL,
+	Borrower_name VARCHAR NOT NULL,
+	Address VARCHAR NOT NULL,
+	Phone VARCHAR NOT NULL,
+	PRIMARY KEY (Card_no)
+);
+
+CREATE TABLE IF NOT EXISTS lib.library_branch (
+	Branch_id INT NOT NULL,
+	Branch_name VARCHAR,
+	Address VARCHAR,
+	PRIMARY KEY (Branch_id)
+);
+
+CREATE TABLE IF NOT EXISTS lib.book (
+	Book_id INT NOT NULL,
+	Title VARCHAR,
+	Publisher_name VARCHAR,
+	PRIMARY KEY (Book_id),
+	FOREIGN KEY (Publisher_name) REFERENCES lib.publisher(Publisher_name)
+);
+
+CREATE TABLE IF NOT EXISTS lib.book_authors (
+	Book_id INT NOT NULL,
+	Author_name VARCHAR NOT NULL,
+	PRIMARY KEY (Book_id), -- should primary key go here or use UNQIUE in Book_id?
+	FOREIGN KEY (Book_id) REFERENCES lib.book(Book_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS lib.book_copies (
+	Book_id INT NOT NULL,
+	Branch_id INT NOT NULL,
+	No_of_copies INT,
+	PRIMARY KEY (Book_id, Branch_id),
+	FOREIGN KEY (Book_id) REFERENCES lib.book(Book_id),
+	FOREIGN KEY (Branch_id) REFERENCES lib.library_branch(Branch_id)
+);
+
+CREATE TABLE IF NOT EXISTS lib.book_loans (
+	Book_id INT NOT NULL,
+	Branch_id INT NOT NULL,
+	Card_no INT NOT NULL,
+	Date_out TIMESTAMP NOT NULL,
+	Due_date TIMESTAMP NOT NULL,
+	FOREIGN KEY (Book_id) REFERENCES lib.book(Book_id),
+	FOREIGN KEY (Branch_id) REFERENCES lib.library_branch(Branch_id),
+	FOREIGN KEY (Card_no) REFERENCES lib.borrower(Card_no)
+);
+
+
