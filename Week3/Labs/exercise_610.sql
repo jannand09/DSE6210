99422f92592efc77bade9627f1d914086965f7aa
jannand09 <josephrannand@gmail.com>
2025-02-01 09:24:09 -0500

    Update exercise_610.sql

--- Week3/Labs/exercise_610.sql
+++ Week3/Labs/exercise_610.sql
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS company.employee (
 	Fname VARCHAR NOT NULL,
 	Minit VARCHAR,
 	Lname VARCHAR NOT NULL,
-	Ssn VARCHAR NOT NULL,
+	Ssn INT NOT NULL,
 	Bdate VARCHAR NOT NULL,
 	Address VARCHAR NOT NULL,
 	Sex VARCHAR NOT NULL,
 	Salary VARCHAR NOT NULL,
-	Super_ssn VARCHAR,
+	Super_ssn INT,
 	Dno INT,
 	PRIMARY KEY (Ssn),
 	FOREIGN KEY (Super_ssn) REFERENCES company.employee(Ssn) ON DELETE SET NULL ON UPDATE CASCADE
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS company.works_on (
 );
 
 CREATE TABLE IF NOT EXISTS company.dependent (
-	Essn VARCHAR NOT NULL,
+	Essn INT NOT NULL,
 	Dependent_name VARCHAR NOT NULL,
 	Sex VARCHAR,
 	Bdate DATE NOT NULL,
@@ -68,3 +68,13 @@ CREATE TABLE IF NOT EXISTS company.dependent (
 	FOREIGN KEY (Essn) REFERENCES company.employee(Ssn) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+INSERT INTO company.employee
+VALUES
+('James', 'E', 'Borg', 888665555, '1937-11-10', '450 Stone, Houston, TX', 'M', 55000, NULL, 1),
+('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20','291 Berry, Bellaire, TX','F',43000,88866555,4),
+('Alicia','J','Zelaya',999887777,'1968-01-19','3321 Castle, Spring, TX','F',25000,987654321,4),
+('Ahmad','V','Jabbar',987987987,'1969-03-29','980 Dallas, Houston, TX','M',25000,987654321,4),
+('Franklin', 'T','Wong',333445555, '1955-12-08', '638 Voss, Houston, TX', 'M', 40000, 888665555, 5),
+('John', 'B','Smith', 123456789, '1965-01-09', '731 Fondren, Houston, TX','M', 30000, 333445555, 5),
+('Ramesh','K','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble, TX','M',38000,333445555,5),
+('Joyce','A','English',453453453,'1972-07-31','5631 Rice, Houston, TX','F',25000,987654321,4)
