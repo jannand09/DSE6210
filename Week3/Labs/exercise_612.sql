1c5dea9c8bec7eb28cb7a824d39713fd625ccdb2
jannand09 <josephrannand@gmail.com>
2025-02-02 13:46:55 -0500

    Update exercise_612.sql
    Insert data staements

--- Week3/Labs/exercise_612.sql
+++ Week3/Labs/exercise_612.sql
@@ -52,3 +52,34 @@ CREATE TABLE IF NOT EXISTS university.prequisite (
 	Prequisite_number VARCHAR,
 	FOREIGN KEY (Course_number,Prequisite_number) REFERENCES university.course(Course_number) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+INSERT INTO university.student
+('Smith',17,1,'CS'),
+('Brown',8,2,'CS');
+
+INSERT INTO university.course
+('Intro to Computer Science','CS1310',4,'CS'),
+('Data Structures','CS3320',4,'CS'),
+('Discrete Mathematics','MATH3320',3,'MATH'),
+('Database','CS3380',3,'CS');
+
+INSERT INTO university.section
+(85,'MATH2410','Fall','07','King'),
+(92,'CS1310','Fall','07','Anderson'),
+(102,'CS3320','Spring','08','Knuth'),
+(112,'MATH2410','Fall','08','Chang'),
+(119,'CS1310','Fall','08','Anderson'),
+(135,'CS3380','Fall','08','Stone');
+
+INSERT INTO university.grade_report
+(17,112,'B'),
+(17,119,'C'),
+(8,85,'A'),
+(8,92,'A'),
+(8,102,'B'),
+(8,135,'A');
+
+INSERT INTO university.prerequisite
+('CS3380','CS3320'),
+('CS3380','MATH2410'),
+('CS3320','CS1310');
